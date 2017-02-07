319f6c4853ac853fa818d648a8e5d04c3573ec09
Kate Hart <codehearts@users.noreply.github.com>
2017-02-07 11:01:56 -0800

    Support aggregated coverage reports
    Count loc and files correctly when classes are present multiple times in the XML

--- cloverpy/clover.py
+++ cloverpy/clover.py
@@ -15,15 +15,56 @@ def convert(coverage_xml):
     tree = ET.parse(coverage_xml).getroot()
     root = tree
     timestamp = root.attrib.get('timestamp')
-    files = len(root.findall('.//class'))
-    loc = len(root.findall('.//line'))
-    elements = len(filter(
-        lambda x: x.attrib.get('hits') == '1',
-        root.findall('.//line')))
+    
+    """
+    Dictionary in the format
+    {
+      (str) class_name: {
+        (str) line_number: (bool) was_line_hit
+      }
+    }
+    """
+    class_files = {}
+
+    # Iterate through all class tags in the XML (some classes may be present multiple times)
+    for class_file in root.findall('.//class'):
+        filename = class_file.get('name')
+
+        # Initialize class names that haven't been seen before
+        if not filename in class_files:
+            class_files[filename] = {}
+
+        # Store the class loc coverage dict
+        class_coverage = class_files[filename];
+
+        # Iterate over all lines of code for this instance of the class in the XML
+        for loc in class_file.findall('.//line'):
+            line_number = loc.get('number')
+
+            # Initialize lines in the code that haven't been seen before as not hit
+            if not line_number in class_coverage:
+                class_coverage[line_number] = False
+
+            # If the line has been hit, mark it as such
+            if loc.get('hits') != '0':
+                class_coverage[line_number] = True
+
+    # Count the unique lines of code and the number of hit lines of code
+    hit_loc = 0
+    total_loc = 0
+    for class_file in class_files.keys():
+        for loc in class_files[class_file].keys():
+            total_loc += 1
+
+            if class_files[class_file][loc]:
+                hit_loc += 1
+
+    class_file_count = len(class_files)
+
     return template.format(**{
         'timestamp': timestamp,
-        'files': files, 'loc': loc,
-        'elements': elements})
+        'files': class_file_count, 'loc': total_loc,
+        'elements': hit_loc})
 
 
 def argument_parser():
