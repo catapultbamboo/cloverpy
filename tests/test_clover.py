e26f8b9c1b7ea35fa41d2c0d555588b2c387212f
Kate Hart <codehearts@users.noreply.github.com>
2017-02-07 11:02:33 -0800

    Fix deprecation warning in test

--- tests/test_clover.py
+++ tests/test_clover.py
@@ -12,7 +12,7 @@ class TestCase(unittest.TestCase):
         clover_xml = clover.convert(coverage)
         root = ET.fromstring(clover_xml)
         timestamp = root.find('./project').attrib.get('timestamp')
-        self.assertEquals(timestamp, '1429279546472')
+        self.assertEqual(timestamp, '1429279546472')
         metrics = root.find('./project/metrics')
         attrib = dict(
             packages="1", files="13", classes="13",
@@ -21,7 +21,7 @@ class TestCase(unittest.TestCase):
             coveredelements="592", coveredstatements="592",
             coveredconditionals="0", conditionals="0",
             methods="0", coveredmethods="0")
-        self.assertEquals(metrics.attrib, attrib)
+        self.assertEqual(metrics.attrib, attrib)
 
 
 if __name__ == '__main__':
