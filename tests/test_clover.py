f468834ae102899f20fddf21d4e06c42a4aba885
Kate Hart <codehearts@users.noreply.github.com>
2017-02-08 09:18:00 -0800

    Added test for aggregate coverage XML

--- tests/test_clover.py
+++ tests/test_clover.py
@@ -23,6 +23,30 @@ class TestCase(unittest.TestCase):
             methods="0", coveredmethods="0")
         self.assertEqual(metrics.attrib, attrib)
 
+    def test_aggregate_convert(self):
+        """
+        Tests converting an XML file containing multiple packages, and
+        multiple class elements for the same file.
+        """
+
+        coverage = os.path.join(os.path.dirname(__file__), 'aggregated-coverage.xml')
+        clover_xml = clover.convert(coverage)
+        root = ET.fromstring(clover_xml)
+
+        # This XML file was generated without a timestamp
+        timestamp = root.find('./project').attrib.get('timestamp')
+        self.assertEqual(timestamp, 'None')
+
+        metrics = root.find('./project/metrics')
+        attrib = dict(
+            packages="2", files="3", classes="3",
+            complexity="0", loc="32", ncloc="32",
+            elements="32", statements="32",
+            coveredelements="23", coveredstatements="23",
+            coveredconditionals="0", conditionals="0",
+            methods="0", coveredmethods="0")
+        self.assertEqual(metrics.attrib, attrib)
+
 
 if __name__ == '__main__':
     unittest.main()
